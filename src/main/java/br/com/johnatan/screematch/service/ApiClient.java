afbaccd13cffaee23b169ac46c5765de34a414b2
Johnatan Vasconcelos <johnatanvasconcelosc@gmail.com>
2025-04-04 14:55:11 -0300

    Refatorando nomes de classes

--- src/main/java/br/com/johnatan/screematch/service/ApiClient.java
+++ src/main/java/br/com/johnatan/screematch/service/ApiClient.java
@@ -7,18 +7,16 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class OmdbAPIClient {
-    public String getDataFromAPI(String endereco) {
+    public String getDataFromAPI(String apiUrl) {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
+                .uri(URI.create(apiUrl))
                 .build();
         HttpResponse<String> response = null;
         try {
             response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
 
